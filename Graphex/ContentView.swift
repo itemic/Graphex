697c04453db38c0b2fb5a9c1fe556ee276bd642d
Terran Kroft <terrankroft@gmail.com>
2021-01-28 19:28:28 +0800

    Initial Commit

---
+++ Graphex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Graphex
+//
+//  Created by Terran Kroft on 1/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Graphex/GraphexApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphexApp.swift
+//  Graphex
+//
+//  Created by Terran Kroft on 1/28/21.
+//
+
+import SwiftUI
+
+@main
+struct GraphexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
